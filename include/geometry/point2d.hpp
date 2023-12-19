be91313dc0b063e1d71ff8736a2f7046460bc4c9
Juyeong Jang <jjo4342@gmail.com>
2023-12-19 23:21:50 +0900

    Made hpp files with doxygen and Header Guard

---
+++ include/geometry/point2d.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file point2d.hpp
+ * @author Juyeong Jang (jjo4342@gmail.com)
+ * @brief
+ * @version 1.0.0
+ * @date 2023-12-19
+ * @copyright Copyright (c) 2023 Juyeong Jang, All Rights Reserved.
+ */
+
+// Copyright (c) 2023 Juyeong Jang, All Rights Reserved.
+// Authors: Juyeong Jang
+
+#ifndef PROGRAMMERS_GEOMETRY_POINT_2D_HPP_
+#define PROGRAMMERS_GEOMETRY _POINT_2D_HPP_
+
+
+
+#endif // PROGRAMMERS_GEOMETRY_POINT_2D_HPP_
