bc1c6dd2592fd51cfe7c125f8c9ed26a9c6d9396
Juyeong Jang <jjo4342@gmail.com>
2023-12-22 20:14:46 +0900

    Added distance hpp/cpp files and point2d cpp file

---
+++ include/geometry/distance.hpp
@@ -0,0 +1,123 @@
+/**
+ * @file geometry/distance.hpp
+ * @author EunSung Yang (seong9115@gmail.com)
+ * @brief Distance class declaration for abstract distance
+ * @version 1.0.0
+ * @date 2023-12-22
+ * @copyright Copyright (c) 2023 HolyGround, All Rights Reserved.
+ */
+
+// Copyright (c) 2023 HolyGround, All Rights Reserved.
+// Authors: EunSung Yang
+
+#ifndef HOLYGROUND__GEOMETRY__DISTANCE_HPP_
+#define HOLYGROUND__GEOMETRY__DISTANCE_HPP_
+
+#include <cstdint>
+
+namespace holyground::geometry {
+/**
+ * @brief The class for abstract distance
+ */
+class Distance {
+ public:
+  /**
+   * @brief The enum class for distance type.
+   */
+  enum class Type {
+    kKilometer = 0,   ///< Kilometer
+    kMeter = 1,       ///< Meter
+    kCentimeter = 2,  ///< Centimeter
+    kMillimeter = 3,  ///< Millimeter
+    kMicrometer = 4,  ///< Micrometer
+    kNanometer = 5    ///< Nanometer
+  };
+
+  /**
+   * @brief Construct a new Distance object.
+   */
+  Distance() = default;
+
+  explicit Distance(double input_value, Type input_type = Type::kMeter);
+
+  /**
+   * @brief The copy constructor.
+   * @param other The other distance object.
+   */
+  Distance(const Distance& other) = default;
+  /**
+   * @brief The move constructor.
+   * @param other The other distance object.
+   */
+  Distance(Distance&& other) noexcept = default;
+
+  /**
+   * @brief Destroy the Distance object.
+   */
+  virtual ~Distance() = default;
+
+  /**
+   * @brief The copy assignment operator.
+   * @param other The other distance object.
+   * @return The reference of distance object.
+   */
+  auto operator=(const Distance& other) -> Distance& = default;
+
+  /**
+   * @brief The move assignment operator.
+   * @param other The other distance object.
+   * @return The reference of distance object.
+   */
+  auto operator=(Distance&& other) -> Distance& = default;
+
+  /**
+   * @brief Get the Distance value for distance type.
+   * @param input_type The distance type.
+   * @return double The distance value.
+   */
+  [[nodiscard]] auto GetValue(const Type& input_type) const -> double;
+
+  /**
+   * @brief Set the Distance value for distance type.
+   * @param input_value The distance value.
+   * @param input_type The distance type.
+   */
+  auto SetValue(double input_value, const Type& input_type) -> void;
+
+  /**
+   * @brief Compare with other distance object for equality.
+   * @param other The other distance object.
+   * @return true If equal.
+   * @return false If not equal.
+   */
+  auto operator==(const Distance& other) const -> bool;
+
+  auto operator!=(const Distance& other) const -> bool;
+  auto operator<(const Distance& other) const -> bool;
+  auto operator<=(const Distance& other) const -> bool;
+  auto operator>(const Distance& other) const -> bool;
+  auto operator>=(const Distance& other) const -> bool;
+
+  /**
+   * @brief Add other distance object.
+   * @param other The other distance object.
+   * @return Distance The result of addition.
+   */
+  auto operator+(const Distance& other) const -> Distance;
+
+  auto operator-(const Distance& other) const -> Distance;
+  auto operator*(double scale) const -> Distance;
+  auto operator/(double scale) const -> Distance;
+  auto operator+=(const Distance& other) const -> void;
+  auto operator-=(const Distance& other) const -> void;
+  auto operator*=(double scale) const -> void;
+  auto operator/=(double scale) const -> void;
+
+ protected:
+ private:
+  int64_t nanometer_{0};  ///< Nanometer
+};
+
+}  // namespace holyground::geometry
+
+#endif  // HOLYGROUND__GEOMETRY__DISTANCE_HPP_
---
+++ src/point2d.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file geometry/src/point2d.cpp
+ * @author EunSung Yang (seong9115@gmail.com)
+ * @brief Point class developments with 2-dimension
+ * @version 1.0.0
+ * @date 2023-12-20
+ * @copyright Copyright (c) 2023 HolyGround, All Rights Reserved.
+ */
+
+// Copyright (c) 2023 HolyGround, All Rights Reserved.
+// Authors: EunSung Yang
+
+#include "geometry/point2d.hpp"
+
+#include <cmath>
+
+namespace programmers::geometry {
+Point2D::Point2D(double input_x, double input_y) : x_(input_x), y_(input_y) {}
+
+auto Point2D::CalculateDistance(const Point2D& target) const -> double {
+  return Point2D::CalculateDistance(*this, target);
+}
+
+auto Point2D::CalculateDistance(const Point2D& lhs, const Point2D& rhs)
+    -> double {
+  return std::sqrt(std::pow((lhs.x_ - rhs.x_), 2) +
+                   std::pow((lhs.y_ - rhs.y_), 2));
+}
+
+auto Point2D::GetX() const -> double { return x_; }
+
+auto Point2D::GetY() const -> double { return y_; }
+
+auto Point2D::SetX(double input_x) -> void { x_ = input_x; }
+
+auto Point2D::SetY(double input_y) -> void { y_ = input_y; }
+}  // namespace holyground::geometry
